8373497a5967570103043b1e43065a8761ec4283
Bill Landolina <bill@Mmaehoos.com>
2026-08-12 15:23:38 -0400

    Add V8 Javascript interpreter and NginX

---
+++ scripts/libv8.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install libv8-dev
+# vim: set ts=8 sw=4 ai expandtab ff=unix :
---
+++ scripts/nginx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install -y nginx
+
+# vim: set ts=8 sw=4 ai expandtab ff=unix :
+
+
